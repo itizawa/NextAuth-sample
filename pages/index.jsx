222a2323df3906aea3823c984f6f1f205b8705db
itizawa <itizawa@weseek.co.jp>
2020-06-14 00:19:43 +0900

    first commit

---
+++ pages/index.jsx
@@ -0,0 +1,15 @@
+import Head from "next/head";
+
+export default () => {
+  return (
+    <div>
+      <Head>Top Page!</Head>
+
+      <main>
+        <h1>
+          Hi there!
+        </h1>
+      </main>
+    </div>
+  );
+};
